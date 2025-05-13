0d4ac7e572cfd0d6ef24e08f6ed06064214490bd
Daulet <youremail@example.com>
2025-05-13 13:03:57 +0500

    fix: corrected the code and removed unnecessary text

--- project3/script.js
+++ project3/script.js
@@ -1,35 +1,3 @@
-{/* <div class="post">
-<div class="info">
-    <div class="author">
-        <img src="img/avatar.png" alt="" class="avatar">
-        <div class="author-text">Authors name <span class="in">in</span> Topics Name · <span class="july">7 July</span></div>
-    </div>
-    <div class="title">7 Practical CSS Tips</div>
-    <div class="title-text-2">
-        Recently, I’ve been automating tasks more than often due to my lack of time. Thanks to that I have 5 new projects that I classified as beginner, intermediate, and advanced.
-        You’ll find links to the full scripts and tutorials to solve each project. Also, I’m leaving a challenge to each of them to test your Python skills.To make things easier, I already created a template for a cover letter. Here’s how the template we’ll use looks...
-    </div>
-    <div class="under-panel">
-        <div class="under-panel-buttons">
-            <div class="javascript">Java Script</div>
-            <div class="min">12 min read</div>
-            <div> · </div>
-            <div class="selected">Selected for you</div>
-        </div>
-        <div class="box">
-            <div class="color-box"></div>
-            <div class="color-box"></div>
-            <div class="color-box"></div>
-        </div>
-    </div>
-</div>
-
-<div class="photo">
-    <img src="img/3.png" alt="" class="photo3">
-</div>
-</div> */}
-
-
 document.addEventListener("DOMContentLoaded", () => {
     loadPosts();
   });
@@ -37,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
   async function loadPosts() {
     try {
       const response = await fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=xnTBQUNTE6hxNFT5jfAqxd4lhYLkNeGp');
-      const data = await response.json(); // получаем всё
-      const posts = data.response.docs;   // достаём массив статей
+      const data = await response.json();
+      const posts = data.response.docs;
   
       console.log(data);
   
@@ -55,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Multimedia:", post.multimedia);
   
         const postElement = document.createElement('div');
-        console.log(post); // Логируем весь пост для отладки
+        console.log(post);
 
         const publishDate = new Date(post.pub_date);
         const formattedDate = publishDate.toLocaleDateString();
